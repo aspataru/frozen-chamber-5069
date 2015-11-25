bc53e2ac53222adb72cf6c24d1df4e0506c0c232
Andrei Spataru <spataruac@gmail.com>
2015-11-25 21:36:01 +0100

    configurable port, mvn exec and package, should be ready for heroku

---
+++ Procfile
@@ -0,0 +1,2 @@
+web:    java $JAVA_OPTS -jar target/parapi-server-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+
--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -11,7 +11,7 @@ public class App {
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
  
-        Server jettyServer = new Server(8080);
+        Server jettyServer = new Server(Integer.valueOf(System.getenv("PORT")));
         jettyServer.setHandler(context);
  
         ServletHolder jerseyServlet = context.addServlet(
